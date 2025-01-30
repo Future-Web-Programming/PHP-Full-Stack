5ed8855ab5fab00aa0de13e8df078aa0c409410f
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2025-01-30 22:14:59 +0500

    Lecture-01 PHP Envoirnment Setup

---
+++ lecture-01-Envoirnment-Setup/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Full Stack Development - Lecture-01</title>
+</head>
+<body>
+    <h1>PHP Full Stack Development</h1>
+    <?php # print("Welcome to PHP Language"); echo "I am the Second"; ?>
+    <!-- this is my commit -->
+    <?php $userName = "Jhon";  ?>
+    <?php echo $userName; ?>
+
+</body>
+</html>
---
+++ lecture-01-Envoirnment-Setup/readme.md
@@ -0,0 +1,5 @@
+# Lecture 01 
+
+in this chapter we have Setup envoirment for to write PHP Code, and Also we have exectued Our First PHP Code.
+
+
