31260288ddfb3b0f43f8749e632c07fbb0e2612b
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-04-25 08:44:57 +0500

    lecture 05 PHP Operators

--- lecture-05-PHP-Operators/index.php
+++ lecture-05-PHP-Operators/index.php
@@ -1,3 +1,50 @@
 <?php 
 
+// $userName = "Jhon";
 
+
+// addition 
+
+// $x = 10;
+// $y = 20;
+
+// $sum = $x + $y;
+
+// echo $sum;
+
+
+// substraciton
+
+// $x = 12;
+// $y = 5;
+
+// $sub = $x - $y;
+
+// echo $sub;
+
+// divsion
+
+// $x = 20;
+// $y = 10;
+
+// $div = $x / $y;
+
+// echo $div;
+
+
+// exponention
+
+// $x = 2;
+// $y = 2;
+
+// $expo = $x ** $y;
+
+// echo $expo;
+
+// $x = 10;
+
+// $y = 3;
+
+// $mod = $x % $y;
+
+// echo $mod;
